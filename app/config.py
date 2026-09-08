bc6c22b20ade1bd188e90378e73b106312a47196
iltafshahni-spe <iltafshahni@gmail.com>
2026-09-08 15:37:37 +0500

    Setup the project

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class settings():
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
+    ACCESS_EXPIRE_TOKEN_TIME = os.getenv("ACCESS_EXPIRE_TOKEN_TIME")
+    SQL_URL = os.getenv("SQL_URL")
+
+settings = settings()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+
 app  = FastAPI
 
 from app.routes.auth import auth_router
